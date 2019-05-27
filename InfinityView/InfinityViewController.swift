5ba84cf10dcbfb79a7d51597c2f4bcdd97b722a1
mohamad <mohamad2@mohamads-mac.local>
2019-05-27 06:15:59 -0700

    ss

--- InfinityView/InfinityViewController.swift
+++ InfinityView/InfinityViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  InfinityViewController.swift
 //  InfinityView
 //
 //  Created by mohamad on 5/15/19.
@@ -8,22 +8,31 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class InfinityViewController: UIViewController {
 
+    @IBOutlet weak var lbl: UILabel!
+    var txt:String = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        // Do any additional setup after loading the view.
+        lbl.text = txt
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBAction func btnBack(_ sender: Any) {
+        _ = navigationController?.popViewController(animated: true)
 
-    @IBAction func btn(_ sender: Any) {
+    }
+    
+    @IBAction func btnTapped(_ sender: Any) {
         let VC1 = self.storyboard!.instantiateViewController(withIdentifier: "InfinityViewController") as! InfinityViewController
+        VC1.txt = "hello"
         self.navigationController!.pushViewController(VC1, animated: true)
     }
     
-}
 
+}
